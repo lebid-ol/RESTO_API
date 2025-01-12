398903141ac7970591ad111dcaa41326d8c3fa49
lebid-ol <swan.programmer@gmail.com>
2025-01-12 15:37:01 +0200

    first commit

---
+++ BankAccounts/Controllers/AccountsController.cs
@@ -0,0 +1,79 @@
+﻿using BankAccounts.Repositories;
+using BankAccounts.RequestModel;
+using BankAccounts.ResponseModels;
+using BankAccounts.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BankAccounts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        // GET: api/<AccountsController>
+        [HttpGet]
+        public ActionResult<string> GetAllAccounts()
+        {
+            var allAccounts = AccountService.GetAccounts();
+
+            if (allAccounts == null)
+            {
+                return NotFound(new { message = "Accounts not found in Database." });
+            }
+
+            return Ok(allAccounts);
+        }
+
+        [HttpGet("query/{id}")]
+        public string GetAllAccountsQuery(
+            [FromRoute] int id,
+            [FromQuery] int[] aId)
+        {
+            if (aId.Length == 0)
+            {
+                return "Missing any aIDS";
+            }
+
+            return $"";
+
+        }
+
+        // GET api/<AccountsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<string> GetAccountById([FromRoute] int id)
+        {
+            var account = AccountService.GetAccount(id);
+
+            if (account == null)
+            {
+                return NotFound(new { message = "Account not found." });
+            }
+
+            return Ok(account);
+        }
+
+        // POST api/<AccountsController>
+        [HttpPost]
+        public ActionResult CreateAccount([FromBody] AccountRequest request)
+        {
+            AccountService.AddAccount(request);
+            return Ok();
+        }
+
+        // PUT api/<AccountsController>/5
+        [HttpPut("{id}")]
+        public string UpdateAccountById([FromRoute] int id, [FromBody] string name)
+        {
+            throw new Exception();
+            return $"Account {id} updated to new name {name}";
+        }
+
+        // DELETE api/<AccountsController>/5
+        [HttpDelete("{id}")]
+        public string DeleteAccountById(int id)
+        {
+            return $"Account with ID {id} was deleted";
+        }
+    }
+}
---
+++ BankAccounts/Repositories/AccountsRepository.cs
@@ -0,0 +1,75 @@
+﻿using BankAccounts.Records;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace BankAccounts.Repositories
+{
+    public static class AccountsRepository
+    {
+        private const string TABLE_NAME = "accounts.csv";
+
+        public static void AddAcountRecord(List<Account> accounts)
+        {
+            if (!File.Exists(TABLE_NAME))
+            {
+                using (var writer = new StreamWriter(TABLE_NAME))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(accounts);
+                }
+            }
+            else 
+            {
+                // Append to the file.
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    // Don't write the header again.
+                    HasHeaderRecord = false,
+                    
+                };
+
+                using (var stream = File.Open(TABLE_NAME, FileMode.Append))
+                using (var writer = new StreamWriter(stream))
+                using (var csv = new CsvWriter(writer, config))
+                {
+                    csv.WriteRecords(accounts);
+                }
+            }
+
+
+        }
+
+        public static Account GetOneAccountFromData(int accountId)
+        {
+            if (!File.Exists(TABLE_NAME))
+            {
+                return null;
+            }
+
+            using (var reader = new StreamReader(TABLE_NAME))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Account>().ToList();
+                return records.FirstOrDefault(account => account.Id == accountId);
+            }
+        }
+
+        public static List<Account> GetAllAccountsFromData()
+        {
+            if (!File.Exists(TABLE_NAME))
+            {
+                return null;
+            }
+
+            using (var reader = new StreamReader(TABLE_NAME))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                return csv.GetRecords<Account>().ToList();
+            }
+        }
+}
+}
+
+
+ 
