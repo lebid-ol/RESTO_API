99b11b221e66fb2b789d7279c7c0b2e31927bfd2
lebid-ol <swan.programmer@gmail.com>
2025-03-30 15:48:14 +0300

    added accounts to user

---
+++ BankAccounts.Shared/Models/UserWithAccounts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts.Shared.Models
+{
+    public class UserWithAccounts
+    {
+        public User User { get; set; }
+        public List<Account> Accounts
+        {
+            get; set;
+        }
+    }
+}
