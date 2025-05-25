c695d719073f9ef5c00d5a35dec3d84616600a1d
Sergei <test@test.com>
2025-05-25 10:17:16 -0500

    http client

--- HttpClientApp/Program.cs
+++ HttpClientApp/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using HttpClientApp;
 
 
 
 var httpClient = new HttpClient();
-
 
 var requestMessage = new HttpRequestMessage();
 
@@ -15,12 +15,38 @@ requestMessage.RequestUri = new Uri("https://localhost:7112/api/accounts");
 requestMessage.Method = new HttpMethod("GET");
 
 
-
 // separate service
 var response = await httpClient.SendAsync(requestMessage);
 
+var test = response.StatusCode;
+
 var jsonString = await response.Content.ReadAsStringAsync();
 var bankResponse = JsonSerializer.Deserialize<List<BankAccountResponse>>(jsonString);
+
+
+// other way
+var responseOther = await httpClient.GetAsync("https://localhost:7112/api/accounts");
+var jsonString2 = await responseOther.Content.ReadAsStringAsync();
+var bankResponse2 = JsonSerializer.Deserialize<List<BankAccountResponse>>(jsonString2);
+
+var createUserRequest = new CreateUserRequest()
+{
+    UserName = "Test",
+    UserLastName = "Test",
+    BillingAddress = "Test",
+    DateOfBirth = "1986-07-07",
+    Email = "Test",
+    Gender = "Man",
+    PhoneNumber = "Test"
+};
+
+var stringRequest = JsonSerializer.Serialize(createUserRequest);
+var stringContent = new StringContent(stringRequest, Encoding.UTF8, "application/json");
+
+var responsePost = await httpClient.PutAsync("https://localhost:7112/api/user", stringContent);
+
+var respCreateUser = await responsePost.Content.ReadAsStringAsync();
+
 
 foreach (var item in bankResponse)
 {
