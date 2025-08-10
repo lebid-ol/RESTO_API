c206a65988956ed941bdef4a5602032dab06ece8
Sergei <test@test.com>
2025-08-10 09:06:23 -0500

    add types

---
+++ BankAccounts.AppplicationData/Migrations/20250810134036_Smth.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankAccounts.AppplicationData.Migrations
+{
+    /// <inheritdoc />
+    public partial class Smth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Transactions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Transactions");
+        }
+    }
+}
