d0f6f55072ea718f9a967412a856868451c44beb
Sergei Belialov <sbelialov@rrc.ca>
2025-11-30 08:56:06 -0600

    add create account

--- PingControllerTests/CustomWebApplicationFactory.cs
+++ PingControllerTests/CustomWebApplicationFactory.cs
@@ -9,6 +9,8 @@ using StackExchange.Redis;
 using System;
 using System.Globalization;
 using System.Threading.Tasks;
+using BankAccounts.Shared.Clients.CurrencyConver;
+using Moq;
 using Testcontainers.PostgreSql;
 using Testcontainers.Redis;
 using Xunit;
@@ -22,18 +24,17 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IAsyn
         .WithImage("postgres:17-alpine")
         .WithDatabase("test_db")
         .WithUsername("test_user")
-        .WithPortBinding(0, 5432)
         .Build();
 
-    private readonly RedisContainer  _redis = new RedisBuilder()
-        .WithImage("redis:7-alpine")
-        .WithPortBinding(0, 6379) // host → container ✅
-        .Build();
+    // private readonly RedisContainer  _redis = new RedisBuilder()
+    //     .WithImage("redis:7-alpine")
+    //     .WithPortBinding(0, 6379) // host → container ✅
+    //     .Build();
 
     public async Task InitializeAsync()
     {
         await _postgres.StartAsync();
-        await _redis.StartAsync();
+        // await _redis.StartAsync();
 
         using var scope = Services.CreateScope();
         var ctx = scope.ServiceProvider.GetRequiredService<PostgresDbContext>();
@@ -49,40 +50,59 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IAsyn
             services.RemoveAll<DbContextOptions<PostgresDbContext>>();
 
             // ✅ Получить тестовый порт контейнера Postgres
-            var mappedPort = _postgres.GetMappedPublicPort(5432);
+            // var mappedPort = _postgres.GetMappedPublicPort(5432);
 
             // ✅ Собрать новый connection string с тестовым портом
-            var connectionString = _postgres.GetConnectionString()
-                .Replace("5432/tcp", $"{mappedPort}/tcp"); // подстраховка если формат меняется
+            var connectionString = _postgres.GetConnectionString();
 
             // ✅ Зарегистрировать DbContext с правильным connection string
             services.AddDbContext<PostgresDbContext>(opt =>
                 opt.UseNpgsql(connectionString));
 
+            
+            
+            // Мокаем ICurrencyConverterClient
+            services.RemoveAll<ICurrencyConverterClient>();
+            var currencyConverterClientMoq = new Mock<ICurrencyConverterClient>();
 
-            // Create Redis multiplexer AFTER container port is mapped
-            var port = _redis.GetMappedPublicPort(6379);
-            var endpoint = $"localhost:{port}";
-            var mux = ConnectionMultiplexer.Connect(new ConfigurationOptions
-            {
-                EndPoints = { endpoint },
-                AbortOnConnectFail = false
-            });
+            currencyConverterClientMoq.Setup(x => x.GetCADRates())
+                .ReturnsAsync(1.5m);
+            
+            services.AddSingleton(currencyConverterClientMoq.Object);
+        
+            services.RemoveAll<IRedisCacheClient>();
+            var redisCacheClienttMoq = new Mock<IRedisCacheClient>();
 
-            // Seed value
-            mux.GetDatabase().StringSet(
-                "Cad_rate", 1.31m.ToString(CultureInfo.InvariantCulture));
-
-            // Register in DI
-            services.AddSingleton<IConnectionMultiplexer>(mux);
+            redisCacheClienttMoq.Setup(x => x.GetCadRate())
+                .ReturnsAsync(1.5m);
+            
+            services.AddSingleton(currencyConverterClientMoq.Object);
+            services.AddSingleton(redisCacheClienttMoq.Object);
+            
+            //
+            // // Create Redis multiplexer AFTER container port is mapped
+            // var port = _redis.GetMappedPublicPort(6379);
+            // var endpoint = $"localhost:{port}";
+            // var mux = ConnectionMultiplexer.Connect(new ConfigurationOptions
+            // {
+            //     EndPoints = { endpoint },
+            //     AbortOnConnectFail = false
+            // });
+            //
+            // // Seed value
+            // mux.GetDatabase().StringSet(
+            //     "Cad_rate", 1.31m.ToString(CultureInfo.InvariantCulture));
+            //
+            // // Register in DI
+            // services.AddSingleton<IConnectionMultiplexer>(mux);
         });
     }
 
     public  async Task DisposeAsync()
     {
-        await _redis.StopAsync();
+        // await _redis.StopAsync();
 
         await _postgres.DisposeAsync();
-        await _redis.DisposeAsync();
+        // await _redis.DisposeAsync();
     }
 }
--- PingControllerTests/UnitTestPing.cs
+++ PingControllerTests/UnitTestPing.cs
@@ -8,6 +8,8 @@ using System.Net;
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
+using BankAccounts.Shared.Models.Requests;
+using BanksAccount.CQRS.Users.Commands.Create;
 using Xunit;
 
 namespace PingControllerTests
@@ -16,7 +18,14 @@ namespace PingControllerTests
     {
         private readonly HttpClient _client;
         private readonly CustomWebApplicationFactory _factory;
-
+        private readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters =
+            {
+                new System.Text.Json.Serialization.JsonStringEnumConverter()
+            }
+        };
 
         public UnitTestPing(CustomWebApplicationFactory factory)
         {
@@ -43,22 +52,44 @@ namespace PingControllerTests
         [Fact]
         public async Task CreateAccount_Success_ReturnsNewAccoutnData()
         {
-            // Act
+            // Arrange
+            var userRequest = new UserRequest()
+            {
+                BillingAddress = "test",
+                DateOfBirth = new DateTime(),
+                Email = "test",
+                Gender = 0,
+                PhoneNumber = "test",
+                UserLastName = "test",
+                UserName = "Test"
+            };
+            
+            var userJson = JsonSerializer.Serialize(userRequest);
+            var userContent = new StringContent(userJson, Encoding.UTF8, "application/json"); // ✅
+            var newUserResponse = await _client.PostAsync("/api/user", userContent);
+            var userResponseString = await newUserResponse.Content.ReadAsStringAsync();
+            
+            var userResponse = JsonSerializer.Deserialize<UserResponse>(userResponseString, _jsonOptions);
+            
             var request = new AccountRequest
             {
                 AccountName = "test name",
                 AccountType = AccountType.Checking,
-                UserId = 1
+                UserId = userResponse.Id
             };
 
             var json = JsonSerializer.Serialize(request);
             var content = new StringContent(json, Encoding.UTF8, "application/json"); // ✅
+            
+            // Act
             var response = await _client.PostAsync("/api/accounts", content);
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            var body = await response.Content.ReadFromJsonAsync<AccountResponse>();
+            var stringBody = await response.Content.ReadAsStringAsync();
+            
+            var body = JsonSerializer.Deserialize<AccountResponse>(stringBody, _jsonOptions);
 
             Assert.NotNull(body);
             Assert.Equal(body.AccountType, AccountType.Checking);
